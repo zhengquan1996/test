46858d71c17d2eb6187a985998cce8a977b7c71d
zhengquan1996 <37835389+zhengquan1996@users.noreply.github.com>
2020-04-21 08:42:54 +0800

    上传文件
    第一次使用github上传文件

---
+++ SpringBoot04WebRestfulcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhengquan.springboot04webrestfulcrud;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot04WebRestfulcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
